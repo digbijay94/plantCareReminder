c612622c09f4524e32d7a9e2ac554420b9a614a7
digbijay94 <beheradigbijay299@gmail.com>
2024-11-12 20:35:26 +0530

    Add files via upload

---
+++ enterpage_plant.js
@@ -0,0 +1,13 @@
+function login1(){
+    window.location="summer_flower.html"
+}
+function login2(){
+    window.location="winterCrops.html"
+}
+function login3(){
+    window.location="rainycrops.html"
+}
+function login10(){
+    window.location="rainycrops.html"
+}
+
